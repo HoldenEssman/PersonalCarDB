0fb8b895ccce1b87dae7b8076540071b4498d6f2
Holden Essman <holdenessman99@gmail.com>
2021-09-14 18:42:57 -0500

    commit

---
+++ carfront/src/components/Login.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import {SERVER_URL} from '../constants.js';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Carlist from './Carlist';
+
+const Login = () => {
+
+    fetch(SERVER_URL + 'login', {
+        method: 'POST',
+        body: JSON.stringify(user)
+    })
+    .then(res => {
+        const jwtToken = res.headers.get('Authorization');
+        if (jwtToken !== null) {
+            sessionStorage.setItem("jwt", jwtToken);
+            setAuth(true);
+        }
+    })
+    .catch(err => console.error(err))
+
+    const [user, setUser] = useState({username: '', password: ''})
+    const [isAuthenticated, setAuth] = useState(false);
+    const handleChange = (event) => {
+        setUser({...user, [event.target.name] : event.target.value})
+        }
+
+if (isAuthenticated === true) {
+    return (<Carlist />)
+    }
+else {
+    return (
+        <div>
+            <TextField name="username"
+                label="Username" onChange={handleChange} /><br/>
+            <TextField type="password" name="password"
+                label="Password" onChange={handleChange} /><br/><br/>
+            <Button variant="outlined" color="primary"
+                onClick={login}>
+                Login
+            </Button>
+        </div>
+    );
+}
